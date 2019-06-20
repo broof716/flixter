ffceed7eaafca15152cc991a908cf3d08b83d5af
Bryan Roof <broof716@gmail.com>
2019-06-20 05:49:39 +0000

    lesson 22 flixter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_214604) do
+ActiveRecord::Schema.define(version: 2019_06_18_175233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 2019_06_10_214604) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.index ["user_id"], name: "index_courses_on_user_id"
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "lessons", id: :serial, force: :cascade do |t|
