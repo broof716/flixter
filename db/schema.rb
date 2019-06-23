872f09398d4d0518018f8dd612ed6fa123d8f1bc
Bryan Roof <broof716@gmail.com>
2019-06-23 21:15:31 +0000

    lesson 22 flixter uodates

--- db/schema.rb
+++ db/schema.rb
@@ -40,18 +40,6 @@ ActiveRecord::Schema.define(version: 2019_06_18_175233) do
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
-
   create_table "sections", id: :serial, force: :cascade do |t|
     t.string "title"
     t.integer "course_id"
